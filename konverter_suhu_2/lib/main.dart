cdbed6e18607b55f5a44cad39427dc7b81117194
haarisahjufri0311 <haarisahjufri0311@gmail.com>
2022-11-15 10:05:15 +0700

    add slider

--- konverter_suhu_2/lib/main.dart
+++ konverter_suhu_2/lib/main.dart
@@ -7,8 +7,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,16 +17,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -36,10 +24,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  double _inputuser = 0;
-  double _kevin = 0;
-  double _reamur = 0;
+//  double _inputuser = 0;
   String newValue = "Kelvin";
   double _result = 0;
   String changeValue = "";
@@ -63,17 +48,6 @@ class _MyHomePageState extends State<MyHomePage> {
         _result = (4 / 5) * _currentSliderValue;
         listViewItem.add("Reamur : " + _result.toString());
       }
-    });
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
     });
   }
 
